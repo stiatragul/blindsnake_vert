00e12a26d8b20d0fc4c64f81bd5969e7711dac6f
Putter Tiatragul <stiatragul@live.com>
2024-03-28 17:27:41 +1100

    functions

--- code/utility/func_vert_profile_plotter.R
+++ code/utility/func_vert_profile_plotter.R
@@ -2,7 +2,7 @@
 
 # Function to plot vertebral profile
 
-vert_profile_plotter <- function(.data1, .data2, .data3, .title = NULL){
+vert_raw_plotter <- function(.data1, .data2, .data3, .title = NULL){
   
   rep1 <- .data1
   rep2 <- .data2
@@ -45,3 +45,60 @@ vert_profile_plotter <- function(.data1, .data2, .data3, .title = NULL){
   # change "My Name & Species name & body length & Number Vertebrae" to your info
   
 }
+
+
+vert_profile_plotter <- function(.data1, .data2, .data3, .title = NULL, .error = FALSE){
+  
+  rep1 <- .data1
+  rep2 <- .data2
+  rep3 <- .data3
+  
+  # Calculate the lengths of each vertebra from the landmark coordinates
+  lindist1 <- diag(as.matrix(dist(rep1))[-1,]) 
+  lindist2 <- diag(as.matrix(dist(rep2))[-1,])
+  lindist3 <- diag(as.matrix(dist(rep3))[-1,])
+  
+  # Combine into a single matrix
+  lindist <- cbind(lindist1, lindist2, lindist3)
+  
+  # Average the vertebrae lengths across the three replicates
+  avg <- apply(lindist, 1, mean)
+  
+  # Calculate the standard deviation for each vertebra length
+  sdev <- apply(lindist, 1, sd)
+  
+  # Calculate the standard error for each relative vertebra length
+  sdevm <- (sdev/sum(sdev)) / sqrt(nrow(lindist))
+  
+  # Calculate relative size
+  relative_size <- avg / sum(avg)
+  
+  
+  # Fit polynomial 4 degree
+  polynomial_fit <- lm(relative_size ~ poly(1:length(relative_size), degree = 4, raw = TRUE))
+  
+  # Generate equally spaced points along the x-axis
+  x_values <- seq(1, length(relative_size), length.out = 100)
+  
+  # Predict corresponding y-values using the fitted polynomial
+  predicted_points <- predict(polynomial_fit, newdata = data.frame(x_values))
+  
+  # Plot the profile
+  plot(relative_size, xlab = "Vertebra Number (index)", ylab = "Relative Vertebra Size")
+  title(paste(.title)) 
+  
+  # Add error bars if .error is TRUE
+  if (.error) {
+    arrows(c(1:length(relative_size)), relative_size-sdevm, 
+           c(1:length(relative_size)), relative_size+sdevm, 
+           length=0.05, angle=90, code=3)
+  }
+  
+  lines(predicted_points, type = "l", col = "red")
+  # legend("topleft", legend = c(" Profile", "Fitted Polynomial"), col = c("black", "red"), lty = c(NA, 1), pch = c(1, NA))
+  
+}
+
+
+
+
